6c1302cb8d075eedc626befa2632fcab273acd16
nikosgks <nikosgkikas@aol.com>
2013-07-20 10:16:41 +0300

    Extended functionality for grader: Added URL check support

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+// RESTLER library
+var restler = require('restler');
+//var sys = require('util');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +58,18 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+// function to handle remotely acquired html
+var checkURL = function(urlcont, checksfile){
+	$ = cheerio.load(urlcont);
+	var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+	return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +80,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <URL>', 'URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	// assuming fixed argument order : --checks <> (--file <> OR --url <>)
+	if (process.argv[4]=='--url' || process.argv[4]=='-u'){
+		var turla = process.argv[5];
+		// no regex mathing for malformed urls for now...
+		restler.get(turla).on('complete', function(result){
+			//sys.puts(result);
+			var checkJson = checkURL(result, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+		});
+	} else {
+	    var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
