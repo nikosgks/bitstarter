64ef66ccbf3ec62aae9d0efb01e2a206db922854
nikosgks <nikosgkikas@aol.com>
2013-07-20 07:52:02 +0300

    Updated web.js to read external file

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+var hcont = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 3!');
+    response.send(hcont.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+//console.log(hcont.toString());
